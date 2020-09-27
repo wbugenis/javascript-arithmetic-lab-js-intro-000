0c70d717e5c0b114baecb027d13baabb68dfd14b
gorgeous-skill-0664 <wbugenis@gmail.com>
2020-09-27 19:51:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,19 @@ function multiply(a, b) {
 function divide(a, b) {
   return (a / b)
 }
+
+function increment(n) {
+  return (n += 1)
+}
+
+function decrement (n) {
+  return (n -= 1)
+}
+
+function makeInt(string) {
+  return (parseInt(string, 10))
+}
+
+function makeFloat(string) new Promise(function(resolve, reject) {
+  return(parseFloat(string))
+}
